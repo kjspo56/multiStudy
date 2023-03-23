acc67bbf77a9f96cf62f171ba15431eb93fcdd7e
tedkim <ted.kim@i-on.net>
2023-03-23 16:44:31 +0900

    basic setting

--- backend/src/main/java/com/roma/study/repository/notice/NoticeQueryDslRepository.java
+++ backend/src/main/java/com/roma/study/repository/notice/NoticeQueryDslRepository.java
@@ -2,6 +2,7 @@ package com.roma.study.repository.notice;
 
 import com.roma.study.dto.notice.NoticeDTO;
 import com.roma.study.dto.page.PageDTO;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
