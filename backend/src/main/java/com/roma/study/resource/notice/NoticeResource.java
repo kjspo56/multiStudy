c8d9b04910f8c65e596a090f0ffcf1c1718eba8f
tedkim <ted.kim@i-on.net>
2023-03-24 17:01:44 +0900

    crud init setting

--- backend/src/main/java/com/roma/study/resource/notice/NoticeResource.java
+++ backend/src/main/java/com/roma/study/resource/notice/NoticeResource.java
@@ -24,8 +24,47 @@ public class NoticeResource {
     @Path("/")
     public Response create(NoticeDTO noticeDTO) {
         return Response.status(Response.Status.OK)
+                .entity(noticeService.create(noticeDTO))
                 .build();
     }
 
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/")
+    public Response update(NoticeDTO noticeDTO){
+        return Response.status(Response.Status.OK)
+                .entity(noticeService.update(noticeDTO))
+                .build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{seq}")
+    public Response get(@PathParam("seq") Long seq) {
+        return Response.status(Response.Status.OK)
+                .entity(noticeService.get(seq))
+                .build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/list")
+    public Response list(){
+        return Response.status(Response.Status.OK)
+                .entity(noticeService.list())
+                .build();
+    }
+
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/delete/{seq}")
+    public Response delete(@PathParam("seq") Long seq){
+        return Response.status(Response.Status.OK)
+                .entity(noticeService.delete(seq))
+                .build();
+    }
 
 }
--- backend/src/main/java/com/roma/study/service/notice/NoticeService.java
+++ backend/src/main/java/com/roma/study/service/notice/NoticeService.java
@@ -1,9 +1,17 @@
 package com.roma.study.service.notice;
 
+import com.roma.study.dto.notice.NoticeDTO;
+import com.roma.study.environment.response.ResponseCode;
+import com.roma.study.model.notice.Notice;
 import com.roma.study.repository.notice.NoticeRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.service.spi.ServiceException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -13,6 +21,57 @@ public class NoticeService {
     private final NoticeRepository noticeRepository;
 
 
+    public Notice create(NoticeDTO noticeDTO) {
+        log.debug("create: {}", noticeDTO);
+        Notice notice = Notice.builder()
+                .title(noticeDTO.title())
+                .content(noticeDTO.content())
+                .build();
+                noticeRepository.save(notice);
+        return notice;
+    }
 
+    @Transactional
+    public Notice update(NoticeDTO noticeDTO){
+        Notice notice = noticeRepository.findById(noticeDTO.seq()).orElseThrow(() -> new ServiceException(ResponseCode.NO_DATA_FOUND.code()));
+        log.debug("find notice Entity : {}", notice);
+        notice.modifyTitleAndContent(noticeDTO.title(), noticeDTO.content());
+        log.debug("modify notice Entity : {}", notice);
+        noticeRepository.save(notice);
+        return notice;
+    }
+
+    @Transactional
+    public NoticeDTO get(Long seq){
+        Notice notice = noticeRepository.findById(seq).orElseThrow(() -> new ServiceException(ResponseCode.NO_DATA_FOUND.code()));
+
+        NoticeDTO noticeDTO = NoticeDTO.builder()
+                .seq(notice.getSeq())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .build();
+        return noticeDTO;
+    }
+
+    public List<NoticeDTO> list() {
+        List<Notice> noticeList = noticeRepository.findAll();
+        List<NoticeDTO> noticeDTOList = new ArrayList<>();
+
+        for(Notice notice : noticeList){
+            NoticeDTO noticeDTO = NoticeDTO.builder()
+                    .seq(notice.getSeq())
+                    .title(notice.getTitle())
+                    .content(notice.getContent())
+                    .build();
+            noticeDTOList.add(noticeDTO);
+        }
+        return noticeDTOList;
+    }
+
+    @Transactional
+    public String delete(Long seq){
+        noticeRepository.deleteById(seq);
+        return ResponseCode.SUCCESS.code();
+    }
 }
 
