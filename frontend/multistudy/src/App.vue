fe74f75fcd55ced9d3705b986b8508bd7e4b7347
tedkim <ted.kim@i-on.net>
2023-03-24 17:24:55 +0900

    [F] Head & Footer

--- frontend/multistudy/src/App.vue
+++ frontend/multistudy/src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+<!--<img alt="Vue logo" src="./assets/logo.png">-->
+  <div id="app">
+    <Header/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Footer/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    HelloWorld,
+    Footer
   }
 }
 </script>
 
 <style>
+html,body{padding:0; margin:0;}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin:0; padding:0;
 }
 </style>
